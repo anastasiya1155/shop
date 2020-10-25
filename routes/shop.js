2c5beb991cef85b039293da62fe4875afb2f53ca
ana1155 <anastasiia.solop@softermii.com>
2020-10-25 14:15:10 +0200

    add ejs

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,9 @@
-const path = require('path');
-
 const express = require('express');
-
-const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', { prods: [], pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
